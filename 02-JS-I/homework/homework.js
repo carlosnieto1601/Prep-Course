ab3cae05a86d51017a44a82f39cace3287652135
Carlos Nieto <marionieto1890@gmail.com>
2022-03-08 18:42:06 -0500

    confirmar

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,6 +28,8 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+  var str='la string provista';
+  return str;
 
 }
 
@@ -35,24 +37,34 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+  var valor = x + y;
+  return valor;
+  
+
 
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  var valor= x-y;
+  return valor;
 
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+ var valor = x*y;
+ return valor;
 
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  var valor = x / y ;
+  return valor;
 
 }
 
@@ -60,6 +72,12 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (x==y) {
+    return true;
+  }else {
+    return false;
+  }
+
 
 }
 
@@ -67,6 +85,11 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(str1.length == str2.length){
+    return true;
+  }else{
+    return false;
+  }
 
 }
 
@@ -74,6 +97,11 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num<90){
+    return true;
+  }else{
+    return false;
+  }
 
 }
 
